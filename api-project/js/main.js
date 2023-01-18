2cb8c7a586a44ddf9a5064e8d1eacf683c391a4b
AbigailLabanok <abigail.labanok@gmail.com>
2023-01-18 13:16:44 -0500

    asda

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,4 +2,8 @@ import "../styles/style.css";
 import { DOMSelectors } from "./DOM";
 import { getPlayerCard } from "./playerCard";
 
-DOMSelectors.getCardBtn.addEventListener("click", getPlayerCard);
+function getCard() {
+  getPlayerCard();
+}
+
+DOMSelectors.getCardBtn.addEventListener("click", getCard);
